8c14ecaadde21cbee75f1838b8dd82c53203712a
qMaccine <qmaccine@gmail.com>
2022-02-21 14:37:34 +0300

    Chatbot \

--- javascript/chat.js
+++ javascript/chat.js
@@ -1,9 +1,24 @@
 $(document).ready(function()
 {
 
-  var user1=document.querySelector("user1");
+  var user1=document.querySelector(".user1");
   user1.addEventListener("click",function()
   {
-  alert("Weeeeee!")
+  $(".bot3").show();
+  $(".user2").hide();
+  $(".horizontal-scroll-wrapper").show();
+  $(".bot5").show();
+  })
+  
+  var user2=document.querySelector(".user2");
+  user2.addEventListener("click",function()
+  {
+ $(".bot4").show();
+ $(".user2").hide();
+ var user1 =document.querySelector(".user1");
+ user1.innerHTML="<p>No</p>";
+ $(".vertical-scroll-wrapper").show();
+ $(".bot6").show();
+ 
   })
 })
